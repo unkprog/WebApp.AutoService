ae8dfc92d0e0d9e7205835aac94e10234cacb0a9
unkprog <unkprog@gmail.com>
2025-12-19 12:38:57 +0300

    Добавление API

---
+++ WebApp.AutoService/api/documents/DocumentsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.AutoService.api.documents
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocumentsController : ControllerBase
+    {
+    }
+}
